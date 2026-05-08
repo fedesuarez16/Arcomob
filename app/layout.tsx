7facc89ea135510267573457753549a007313e35
fedesuarez16 <fedesuarez16@gmail.com>
2026-05-08 03:30:58 -0300

    idk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import './globals.css'
 import Footer from '@/components/Footer'
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-CDCQWD09GZ" />
     </html>
   )
 }
